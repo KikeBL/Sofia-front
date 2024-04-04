a9f7f409587799eb73e1c9b203f74d861faa8006
KikeBL <enriquebarrerol@gmail.com>
2024-04-04 18:40:41 +0200

    Sign in and Sign up styles

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,20 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				'sofia-purple': '#BD2CBF',
+				'sofia-pink': '#BF2489',
+				'sofia-yellow': '#ffe100',
+				'sofia-blue': '#0B438C',
+				'sofia-error': '#DD1805',
+				'sofia-success': '#17E788',
+			},
+
+			fontFamily: {
+				'tilt-neon': ['Tilt Neon Variable', 'system-ui']
+			}
+		},
 	},
 	plugins: [],
 }
